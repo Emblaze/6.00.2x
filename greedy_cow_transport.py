48d550e4340e10cd78ce35545d6220c9b6f09ecb
Emblaze <benjamin.masse@gmail.com>
2018-12-18 23:52:59 +0100

    Simpler dict sorting and conversion into a list, Fix logic and appending

--- greedy_cow_transport.py
+++ greedy_cow_transport.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
-def greedy_cow_transport(cows, limit=10):
+
+
+def greedy_cow_transport(d, limit=10):
     """
-    Input: a dict containing names as keys and weights as values
-    Output: a list of lists containing cow names that are below
-    a limit weight
+    Input: a dict of name:weight pairs
+    Output: a list sublist of names which total weight is under a limit weight
     """
-    names = sorted(cows, key=cows.get, reverse=True)
-    weights = sorted(cows.values(), reverse=True)
+    cows = sorted([t for t in d.items()], key=lambda x: x[1], reverse=True)
+
     trips = []
-
-    for cow in cows:
-        # print('Outer loop', i)
-        totalWeight = 0
-        for n, w in zip(names, weights):
-            # print('name', n, 'weight', w)
-            goodToShip = []
-
-            if totalWeight + w <= limit:
-                print(n, 'tw', totalWeight, '+', w, end=' ')
-                totalWeight += w
-                print('=', totalWeight)
-                goodToShip.extend([n])
-                print('Shipping', n, w, goodToShip)
-                names.pop(names.index(n))
-                weights.pop(weights.index(w))
-            # trips.append(goodToShip)
-
-    return trips
+    total = 0
+    trip = []
+    while cows:
+        for i in range(len(cows)):
+            if total + cows[i][1] <= limit:
+                total += cows[i][1]
+                trip += [cows[i][0]]
+                cows.pop(i)
+                break
+        else:
+            trips += [trip]
+            total = 0
+            trip = []
+    return trips if not trip else trips + [trip]
 
 
-cows = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
-print(greedy_cow_transport(cows))
+# cows = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
+print(greedy_cow_transport({"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}, 10))
+
+print(greedy_cow_transport({'Polaris': 20, 'Horns': 50, 'MooMoo': 85, 'Louis': 45, 'Patches': 60, 'Clover': 5, 'Miss Bella': 15, 'Muscles': 65, 'Milkshake': 75, 'Lotus': 10}, 100))
+
+print(greedy_cow_transport({'Coco': 10, 'Rose': 50, 'Patches': 12, 'Willow': 35, 'Betsy': 65, 'Lilly': 24, 'Abby': 38, 'Daisy': 50, 'Buttercup': 72, 'Dottie': 85}, 100))
+
+print(greedy_cow_transport({'Rose': 42, 'Willow': 59, 'Betsy': 39, 'Abby': 28, 'Coco': 59, 'Buttercup': 11, 'Luna': 41, 'Starlight': 54}, 120))
