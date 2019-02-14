eb2b2f89530688af73836ddf2e42821bb489c5dd
Emblaze <benjamin.masse@gmail.com>
2019-02-14 23:59:28 +0100

    Update greedy, Add brute_force_cow_transport, compare_cow_transport_algorithms

--- brute_force_cow_transport.py
+++ brute_force_cow_transport.py
@@ -3,10 +3,27 @@ import ps1_partition
 
 
 def brute_force_cow_transport(cows, limit=10):
-    cowsList = [t for t in cows.items()]
-    for partition in ps1_partition.get_partitions(cowsList):
-        print(partition)
+    """
+    Implement a brute force algorithm to find the minimum number of trips
+    needed to take all the cows across the universe in the function
+    brute_force_cow_transport under a limit weight constraint.
 
+    Input: a dict of name:weight pairs
 
-cows = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
-print(brute_force_cow_transport(cows))
+    Output: a list of lists of names where each inner list represents a trip
+    and contains the names of cows taken on that trip.
+    """
+    valid_trips = []
+
+    def weight(subpart):
+        sum = 0
+        for i in subpart:
+            sum += cows[i]
+        return sum
+
+    for part in list(ps1_partition.get_partitions(cows)):
+        if all(weight(subpart) <= limit for subpart in part):
+            valid_trips.append(part)
+    return min(valid_trips)
+
+# print(brute_force_cow_transport({"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}, 10))
--- greedy_cow_transport.py
+++ greedy_cow_transport.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 
-def greedy_cow_transport(d, limit=10):
+def greedy_cow_transport(cows, limit=10):
     """
     Input: a dict of name:weight pairs
     Output: a list sublist of names which total weight is under a limit weight
     """
-    cows = sorted([t for t in d.items()], key=lambda x: x[1], reverse=True)
+    cows = sorted([t for t in cows.items()], key=lambda x: x[1], reverse=True)
 
     trips = []
     total = 0
@@ -26,10 +26,10 @@ def greedy_cow_transport(d, limit=10):
 
 
 # cows = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
-print(greedy_cow_transport({"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}, 10))
+# print(greedy_cow_transport({"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}, 10))
 
 # print(greedy_cow_transport({'Polaris': 20, 'Horns': 50, 'MooMoo': 85, 'Louis': 45, 'Patches': 60, 'Clover': 5, 'Miss Bella': 15, 'Muscles': 65, 'Milkshake': 75, 'Lotus': 10}, 100))
 #
 # print(greedy_cow_transport({'Coco': 10, 'Rose': 50, 'Patches': 12, 'Willow': 35, 'Betsy': 65, 'Lilly': 24, 'Abby': 38, 'Daisy': 50, 'Buttercup': 72, 'Dottie': 85}, 100))
-# 
+#
 # print(greedy_cow_transport({'Rose': 42, 'Willow': 59, 'Betsy': 39, 'Abby': 28, 'Coco': 59, 'Buttercup': 11, 'Luna': 41, 'Starlight': 54}, 120))
