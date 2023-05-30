9099ade80c547f53dca98a6f5a3a2b1161598335
Emblaze <benjamin.masse@gmail.com>
2023-05-30 12:48:10 +0200

    Fix runSimulation. Grader not happy :-)

--- pset2/ps2.py
+++ pset2/ps2.py
@@ -243,7 +243,7 @@ class StandardRobot(Robot):
             # Next position is in the room, let's move to it
             self.position = self.next_position
         # Debug check to determine whether a tile has already been cleaned.
-        print("Tile", ((int(self.position.x)), (int(self.position.y))), "already cleaned:", bool(self.room.isTileCleaned((int(self.position.x)), (int(self.position.y)))))
+        # print("Tile", ((int(self.position.x)), (int(self.position.y))), "already cleaned:", bool(self.room.isTileCleaned((int(self.position.x)), (int(self.position.y)))))
         # Clean tile at new position
         self.room.cleanTileAtPosition(self.position)
 
@@ -270,79 +270,60 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     num_trials: an int (num_trials > 0)
     robot_type: class of robot to be instantiated (e.g. StandardRobot or RandomWalkRobot)
     """
-    for trial in range(num_trials):
-        print(end="________\n")   
-        print("Trial #:", trial+1)
-        print(end="________\n")    
-        num_robots = 1
-        
-        # Create an instance of a room
-        room = RectangularRoom(width, height)
-        print("Room is an instance of RectangularRoom:", isinstance(room, RectangularRoom), end = ". ")
-        print("# of tiles:", f"{room.getNumTiles()}", end = ". ")
+    num_robots = 1
+    stepList = []
+    counter = 0
+    # Run 1 simulation
+    def runOneTrial():
         # Create an instance of a robot of robot_type with initial random position and direction
         robot = robot_type(room, speed)
-        print("Robot is an instance of Robot:", isinstance(robot, Robot), end = ". ")
+        # print("Robot is an instance of Robot:", isinstance(robot, Robot), end = ". ")
         position = robot.getRobotPosition()
         direction = robot.getRobotDirection()
-        print(f"{position}", "is an instance of Position:", isinstance(position, Position), vars(position))
-        print("Initial position:", f"{position}", "| Heading:", f"{direction}" + '°')
-        counter = 0
-        stepList = []
-        #runOneTrial()
+        # print(f"{position}", "is an instance of Position:", isinstance(position, Position), vars(position))
+        # print("Position:", f"{position}", "| Heading:", f"{direction}" + '°', end="\n")
+        # Build a list of cleaned tiles
+        cleanedTiles = [tile for tile,state in room.tiles.items() if state == True]
+        ratio = room.getNumCleanedTiles() / room.getNumTiles()
+        # Move to next position
+        robot.updatePositionAndClean()
+        # print("Position now set to", robot.getRobotPosition(), "| Heading:", f"{robot.getRobotDirection()}" + '°', end=". ")
+        # print("Clean tiles:", room.getNumCleanedTiles(), cleanedTiles)
+        cleanedTiles = [tile for tile,state in room.tiles.items() if state == True]
+        nonlocal counter
+        counter += 1
+        # print("Ratio:", round(ratio,2),  "< min_coverage:", str(min_coverage), bool(ratio<min_coverage), end="\n")
+        # print("Steps:", counter, end=". ")
+        return ratio
+    
 
-        # Run 1 simulation
-        def runOneTrial(ratio = 0):
-            # Build a list of cleaned tiles
-            cleanedTiles = [tile for tile,state in room.tiles.items() if state == True]
-            ratio = room.getNumCleanedTiles() / room.getNumTiles()
-           
-            # Move to next position
-            robot.updatePositionAndClean()
-            print("Position now set to", robot.getRobotPosition(), "| Heading:", f"{robot.getRobotDirection()}" + '°', end = ". ")
-        
-            print("Clean tiles:", room.getNumCleanedTiles(), cleanedTiles)
-
-            cleanedTiles = [tile for tile,state in room.tiles.items() if state == True]
-
-            print("Ratio:", round(ratio,2), "Steps:", counter, end=". ")
-            print("Ratio < min_coverage:", bool(ratio<min_coverage), end="\n")
-            return ratio
-        
-        
+    for trial in range(num_trials):
+        # print(end="________\n")   
+        print("Trial #:", trial+1)
+        # Create an instance of a room
+        room = RectangularRoom(width, height)
+        # print("Room is an instance of RectangularRoom:", isinstance(room, RectangularRoom), end = ". ")
+        # print("# of tiles:", f"{room.getNumTiles()}")  
         while runOneTrial() < min_coverage:
             runOneTrial()
-            counter += 1
-
-        print("Appending", f"{counter}", "steps", type(counter))
-        stepList.append(counter,)
-         
-    
-    print("________")
-    print("Results:")
-    print("Steps:", stepList, type(stepList), end= "\n",)
+        print("Appending", f"{counter}", "steps")
+        stepList.append(counter)
+        # Resetting counter
+        counter = 0
+        
+    # print("________")
+    # print("Results:")
+    print("Steps per trial:", stepList, len(stepList), "items", end= "\n")
     mean = sum(stepList)/ len(stepList)
-    print("Mean:", round(mean, 2))
+    print("Mean:", end= " ")
     return round(mean, 2)
                    
 
 # Build-Debug
 # random.seed(0)
-# robot.setRobotPosition(position)
-# direction = random.randint(0, 359)
-# robot.setRobotDirection(direction)
-# print("Position now set to", robot.getRobotPosition(), "| Heading:", f"{robot.getRobotDirection()}" + '°', end = ". ")
-# print("Tile is clean:", room.isTileCleaned(int(position.x), int(position.y)))
-# room.cleanTileAtPosition(position)
-# print("Tile", f"{position}", "is clean:", room.isTileCleaned(int(position.x), int(position.y)))
-# position =  position.getNewPosition(direction, 1)
-# print("Position:", f"{position}", "is in the room:", room.isPositionInRoom(position))
-# print("Position variables:", vars(position))
-# print(vars(robot))
-# print(vars(room))
 
 # Uncomment this line to see how much your simulation takes on average
-print(runSimulation(1, 1.0, 5, 5, 0.75, 3, StandardRobot))
+print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
 
 
 # === Problem 5
@@ -358,8 +339,8 @@ class RandomWalkRobot(Robot):
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        # self.position = self.room.getRandomPosition() # This should be implemented in the RandomWalkRobot
-        raise NotImplementedError
+        self.direction = random.randint(0, 359)
+        self.robot.setRobotDirection(self.direction)
 
 
 def showPlot1(title, x_label, y_label):
